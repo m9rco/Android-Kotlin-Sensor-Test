44b5a1dd57d5c72730d18557d96aed56de7cd989
sandeeptile <sandeeptile@gmail.com>
2018-08-06 15:09:58 +0530

    ACCELEROMETER working

--- app/src/main/java/com/example/sandy/kotlin_sensor_test/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlin_sensor_test/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.sandy.kotlin_sensor_test
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        var sManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        var sensor=sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        sManager.registerListener(object :SensorEventListener{
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+
+                var values:FloatArray = event!!.values
+                textView.text = values[0].toString()
+                textView2.text = values[1].toString()
+
+            }
+
+
+        },sensor,SensorManager.SENSOR_DELAY_NORMAL)
 
     }
 }
